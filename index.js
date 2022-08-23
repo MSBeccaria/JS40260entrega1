bbd6d3a1a058448337e8a4fb695caa71e77d8592
Soledad Beccaria <mariasoledadbeccaria@gmail.com>
2022-08-23 15:51:14 -0300

    index html y js

---
+++ index.js
@@ -0,0 +1,8 @@
+let envio = prompt("Indica a cuantos km vives desde zona centro de CABA?");
+
+while(envio > 20){
+    alert("No hacemos envíos fuera de CABA");
+    envio = prompt("Indica a cuantos km vives desde zona centro de CABA?");
+}
+    alert("El envío cuesta $1000")
+
